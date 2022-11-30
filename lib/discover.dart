3db79170f8991b862f10eb5f01dd924d949dc435
Mateo THIEBAULT <mateo.thiebault@epsi.fr>
2022-11-30 23:10:01 +0100

    add google maps

--- lib/discover.dart
+++ lib/discover.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class Discover extends StatefulWidget {
   const Discover({super.key});
@@ -8,8 +11,39 @@ class Discover extends StatefulWidget {
 }
 
 class _DiscoverState extends State<Discover> {
+  late GoogleMapController mapController;
+  Map<String, Marker> _markers = {};
+
   @override
   Widget build(BuildContext context) {
-    return const Center(child: Text('Discover'));
+    return Scaffold(
+      body: GoogleMap(
+        initialCameraPosition:
+            const CameraPosition(target: LatLng(43.610769, 3.876716), zoom: 14),
+        onMapCreated: ((controller) {
+          mapController = controller;
+          addMarkers();
+        }),
+        markers: _markers.values.toSet(),
+      ),
+    );
+  }
+
+  addMarkers() {
+    _markers["marcheDuLez"] = (const Marker(
+        markerId: MarkerId("marcheDuLez"),
+        position: LatLng(43.592886861946546, 3.9055549122740523),
+        infoWindow: InfoWindow(title: "Marché du Lez", snippet: "C'est bon")));
+    _markers["peyrou"] = (const Marker(
+        markerId: MarkerId("peyrou"),
+        position: LatLng(43.61133880197438, 3.8707500617576316),
+        infoWindow: InfoWindow(title: "Peyrou", snippet: "Un grand espace")));
+    _markers["esplanade"] = (const Marker(
+        markerId: MarkerId("esplanade"),
+        position: LatLng(43.61089552994617, 3.881532498367065),
+        infoWindow: InfoWindow(
+            title: "L'Esplanade", snippet: "Une belle ligne droite")));
+
+    setState(() {});
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -73,8 +73,7 @@ class _HomeState extends State<Home> {
           BottomNavigationBarItem(
               icon: Icon(Icons.skateboarding), label: 'List'),
           BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Search'),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.task_alt), label: 'Discover'),
+          BottomNavigationBarItem(icon: Icon(Icons.map), label: 'Map'),
         ],
         currentIndex: _selectedIndex,
         selectedItemColor: const Color(0xffFF914D),
