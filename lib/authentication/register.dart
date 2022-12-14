625178c05f118ec6ada520278f6387b139ffa853
Thiebault Mateo <mateo.thiebault@genapi.fr>
2022-12-14 15:53:40 +0100

    adding registeration error snackbar

--- lib/authentication/register.dart
+++ lib/authentication/register.dart
@@ -2,6 +2,7 @@ import 'package:e_skate/sharded/global.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:email_validator/email_validator.dart';
 
 class Register extends StatefulWidget {
   final VoidCallback onclickedLogin;
@@ -12,6 +13,7 @@ class Register extends StatefulWidget {
 }
 
 class _RegisterState extends State<Register> {
+  final formKey = GlobalKey<FormState>();
   final loginController = TextEditingController();
   final passwordController = TextEditingController();
   final confirmPasswordController = TextEditingController();
@@ -21,64 +23,88 @@ class _RegisterState extends State<Register> {
       margin: const EdgeInsets.all(20),
       child: SingleChildScrollView(
         physics: const BouncingScrollPhysics(),
-        child: Column(
-          children: [
-            Container(
-              margin: const EdgeInsets.only(top: 100),
-              width: 150,
-              height: 150,
-              decoration: BoxDecoration(
-                image:
-                    const DecorationImage(image: AssetImage('assets/logo.png')),
-                border: Border.all(color: Colors.white, width: 5),
-                borderRadius: BorderRadius.circular(100),
+        child: Form(
+          key: formKey,
+          child: Column(
+            children: [
+              Container(
+                margin: const EdgeInsets.only(top: 100),
+                width: 150,
+                height: 150,
+                decoration: BoxDecoration(
+                  image: const DecorationImage(
+                      image: AssetImage('assets/logo.png')),
+                  border: Border.all(color: Colors.white, width: 5),
+                  borderRadius: BorderRadius.circular(100),
+                ),
               ),
-            ),
-            TextField(
-              controller: loginController,
-              decoration: const InputDecoration(labelText: 'Username'),
-            ),
-            TextField(
-              controller: passwordController,
-              obscureText: true,
-              decoration: const InputDecoration(labelText: 'Password'),
-            ),
-            TextField(
-              controller: confirmPasswordController,
-              obscureText: true,
-              decoration: const InputDecoration(labelText: 'Confirm Password'),
-            ),
-            const SizedBox(height: 50),
-            ElevatedButton(
-              onPressed: register,
-              style: ElevatedButton.styleFrom(
-                  backgroundColor: globalColor,
-                  minimumSize: const Size.fromHeight(50)),
-              child: const Text('Sign Up'),
-            ),
-            Container(
-              margin: const EdgeInsets.only(top: 15),
-              child: RichText(
-                  text: TextSpan(text: '', children: [
-                TextSpan(
-                    recognizer: TapGestureRecognizer()
-                      ..onTap = widget.onclickedLogin,
-                      style: (const TextStyle(color: Colors.grey, fontWeight: FontWeight.bold)),
-                    text: 'Sign In')
-              ])),
-            ),
-          ],
+              TextFormField(
+                controller: loginController,
+                decoration: const InputDecoration(labelText: 'Username'),
+                autovalidateMode: AutovalidateMode.onUserInteraction,
+                validator: (login) =>
+                    login != null && !EmailValidator.validate(login)
+                        ? 'Enter a valid email'
+                        : null,
+              ),
+              TextFormField(
+                  controller: passwordController,
+                  obscureText: true,
+                  decoration: const InputDecoration(labelText: 'Password'),
+                  autovalidateMode: AutovalidateMode.onUserInteraction,
+                  validator: (password) =>
+                      password != null && password.length < 6
+                          ? 'Enter a password with more than 6 characters'
+                          : null),
+              TextFormField(
+                  controller: confirmPasswordController,
+                  obscureText: true,
+                  decoration:
+                      const InputDecoration(labelText: 'Confirm Password'),
+                  autovalidateMode: AutovalidateMode.onUserInteraction,
+                  validator: (validationPassword) =>
+                      validationPassword != null &&
+                              validationPassword != passwordController.text
+                          ? "The two passwords don't match"
+                          : null),
+              const SizedBox(height: 50),
+              ElevatedButton(
+                onPressed: register,
+                style: ElevatedButton.styleFrom(
+                    backgroundColor: globalColor,
+                    minimumSize: const Size.fromHeight(50)),
+                child: const Text('Sign Up'),
+              ),
+              Container(
+                margin: const EdgeInsets.only(top: 15),
+                child: RichText(
+                    text: TextSpan(text: '', children: [
+                  TextSpan(
+                      recognizer: TapGestureRecognizer()
+                        ..onTap = widget.onclickedLogin,
+                      style: (const TextStyle(
+                          color: Colors.grey, fontWeight: FontWeight.bold)),
+                      text: 'Sign In')
+                ])),
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
 
   Future register() async {
+    final isValid = formKey.currentState!.validate();
+    if (!isValid) return;
+
     try {
       await FirebaseAuth.instance.createUserWithEmailAndPassword(
           email: loginController.text, password: passwordController.text);
     } on FirebaseAuthException catch (e) {
       print(e);
+      final snackbar = SnackBar(content: Text(e.message!), backgroundColor: globalColor);
+      ScaffoldMessenger.of(context).showSnackBar(snackbar);
     }
   }
 }
